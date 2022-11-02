bfa98d54c1e95f42d3dee3195230cc25535aaab4
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2022-11-02 13:11:46 -0700

    added score and timer

--- memory-game.js
+++ memory-game.js
@@ -9,8 +9,14 @@ const COLORS = [
 ];
 
 const colors = shuffle(COLORS);
+let score = 0;
+console.log(score)
+let scoreHi = 0;
+let time = 0;
 
 createCards(colors);
+createScoreBoard();
+console.log(colors);
 
 
 /** Shuffle array items in-place and return shuffled array. */
@@ -24,29 +30,29 @@ function shuffle(items) {
 
   return items;
 }
-console.log(colors);
 
-/** Create card for every color in colors (each will appear twice)
- *
- * Each div DOM element will have:
- * - a class with the value of the color
- * - a click event listener for each card to handleCardClick
- */
+//create cards. respond to first click, second click matchd, and second click unmatched.
 
 function createCards(colors) {
   const gameBoard = document.getElementById("game");
+
+
 
   let cardClicked = 0;
   let cardColor = undefined;
   let cardLast = undefined;
 
+
   for (let color of colors) {
+    //create initial unflipped cards
     const card = document.createElement("div");
     card.style.backgroundColor = 'black';
 
+    //create three click pathways
     card.addEventListener("click", handleCardClick)
     function handleCardClick() {
 
+      //action if first card flipped
       if (cardClicked === 0) {
         card.style.backgroundColor = `${color}`;
         cardColor = `${color}`
@@ -57,7 +63,7 @@ function createCards(colors) {
         console.log(cardLast)
       }
 
-
+      //if second card flipped isn't a match, unflip both
       else if (cardClicked === 1 && cardColor !== color) {
         card.style.backgroundColor = `${color}`;
         setTimeout(function () {
@@ -67,9 +73,9 @@ function createCards(colors) {
           cardClicked = 0;
           cardLast = undefined;
         }, 1000);
-
       }
 
+      //if second card flipped is a match leave flipped
       else if (cardClicked === 1 && cardColor === color && card !== cardLast) {
         card.style.backgroundColor = `${color}`;
         card.removeEventListener("click", handleCardClick);
@@ -77,14 +83,68 @@ function createCards(colors) {
         cardColor = undefined;
         cardClicked = 0;
         cardLast = undefined;
-
       }
+      score++;
+      document.getElementById("score-text").innerHTML = "Amount of Guesses: " + score;
+      console.log(score);
 
     };
     gameBoard.appendChild(card);
   }
 
 }
+
+
+function createScoreBoard() {
+  const scoreBoard = document.getElementById("scores");
+  let scoreBoardDiv = document.createElement("div");
+  let scoreText = document.createElement("p");
+  scoreText.setAttribute("id", "score-text");
+  let scoreHiText = document.createElement("p");
+  let timer = document.createElement("p");
+  timer.setAttribute("id", "timer");
+  scoreHiText.setAttribute("id", "score-hi-text");
+  scoreText.innerHTML = "Amount of Guesses: " + score;
+  scoreHiText.innerText = scoreHi;
+  timer.innerHTML = "Time: " + time + " seconds";
+  scoreBoardDiv.appendChild(timer);
+  scoreBoardDiv.appendChild(scoreText);
+  scoreBoardDiv.appendChild(scoreHiText);
+  scoreBoard.appendChild(scoreBoardDiv);
+}
+
+// function clock() {
+setInterval(function () { time++; document.getElementById("timer").innerHTML = "Time: " + time + " seconds" }, 1000);
+
+
+
+// function createScoreBoard(score, hiScore) {
+//   let scoreBoard = document.createElement("div");
+//   let score = document.createElement("p");
+//   let hiScore = document.createElement("p");
+//   score.value = score;
+//   hiScore.value = hiScore;
+//   scoreBoard.appendChild(score);
+//   scoreBoard.appendChild(hiScore);
+//   gameBoard.appendChild(scoreBoard);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /** Flip a card face-up. */
 
